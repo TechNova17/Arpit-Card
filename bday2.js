c8249d0f33a52d9e4d7a3ef6cdc861098d789ff5
TechNova <arpitrajcoding@gmail.com>
2026-09-11 17:19:07 +0000

    All Details Updated

--- bday2.js
+++ bday2.js
@@ -11,10 +11,10 @@
 const CONFIG = {
 
   // Dayy of the month being celebrated (1–31)
-  day: 13,
+  day: 28,
 
   // The birthday person's name, shown on the memory wall and letter
-  name: "MAHIMA",
+  name: "THAKUR BHAI",
 
   // The letter message. Use \n for line breaks.
   message:
